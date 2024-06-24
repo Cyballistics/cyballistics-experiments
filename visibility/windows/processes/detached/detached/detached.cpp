6dae024c8eb11db895bc8ca17d29e07acf7b2fe5
dfirence <carlos.diaz.pro@gmail.com>
2024-06-24 18:20:46 -0400

    license

--- visibility/windows/processes/detached/detached/detached.cpp
+++ visibility/windows/processes/detached/detached/detached.cpp
@@ -1,3 +1,40 @@
+/**
+* 
+* Copyright (c) 2024-present, Cyballistics, LLC. All Rights Reserved.
+* 
+* License      The MIT License
+* 
+*-------------------------------------------------------------------------------------
+* Author       carlos_diaz | @dfirence
+* About        Applied Conceptual Usage of AMSI API.
+* Purpose      Non Production grade, testing and teaching
+*
+* ------------------------------------------------------------------------------------
+* 
+* This program is released as a teaching instrument for practical usage.
+* The student will learn the key aspects of initiating and disposing of resources 
+* associated with the AMSI API - Antimalware Scanning Interface.
+* 
+* ------------------------------------------------------------------------------------
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+* ------------------------------------------------------------------------------------
+*/
 #include <windows.h>
 #include <iostream>
 
